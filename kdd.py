6ef5733808595c69b32a07f2912681044103c987
Dhruv Solanki <dhruvsolanki34@gmail.com>
2020-01-14 11:22:42 +0530

    Add files via upload
    Python file which contain main code

---
+++ kdd.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 10:39:05 2019
+
+@author: DHRUV
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("KDDTest+.csv")
+
+# attach the column names to the dataset
+col_names = ["duration","protocol_type","service","flag","src_bytes",
+    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
+    "logged_in","num_compromised","root_shell","su_attempted","num_root",
+    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
+    "is_host_login","is_guest_login","count","srv_count","serror_rate",
+    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
+    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
+    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
+    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
+    "dst_host_rerror_rate","dst_host_srv_rerror_rate","label"]
+
+# KDDTrain+_2.csv & KDDTest+_2.csv are the datafiles without the last column about the difficulty score
+# these have already been removed.
+df = pd.read_csv("KDDTrain+.csv", header=None, names = col_names)
+df_test = pd.read_csv("KDDTest+.csv", header=None, names = col_names)
+
+# shape, this gives the dimensions of the dataset
+print('Dimensions of the Training set:',df.shape)
+print('Dimensions of the Test set:',df_test.shape)
+
+df.head(5)
+
+# colums that are categorical and not binary yet: protocol_type (column 2), service (column 3), flag (column 4).
+# explore categorical features
+print('Training set:')
+for col_name in df.columns:
+    if df[col_name].dtypes == 'object' :
+        unique_cat = len(df[col_name].unique())
+        print("Feature '{col_name}' has {unique_cat} categories".format(col_name=col_name, unique_cat=unique_cat))
+
+
+#see how distributed the feature service is, it is evenly distributed and therefore we need to make dummies for all.
+print()
+print('Distribution of categories in service:')
+print(df['service'].value_counts().sort_values(ascending=False).head())
+
+
+# Test set
+print('Test set:')
+for col_name in df_test.columns:
+    if df_test[col_name].dtypes == 'object' :
+        unique_cat = len(df_test[col_name].unique())
+        print("Feature '{col_name}' has {unique_cat} categories".format(col_name=col_name, unique_cat=unique_cat))
+        
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+categorical_columns=['protocol_type', 'service', 'flag']
+# insert code to get a list of categorical columns into a variable, categorical_columns
+categorical_columns=['protocol_type', 'service', 'flag'] 
+ # Get the categorical values into a 2D numpy array
+df_categorical_values = df[categorical_columns]
+testdf_categorical_values = df_test[categorical_columns]
+df_categorical_values.head()
+
+# protocol type
+unique_protocol=sorted(df.protocol_type.unique())
+string1 = 'Protocol_type_'
+unique_protocol2=[string1 + x for x in unique_protocol]
+# service
+unique_service=sorted(df.service.unique())
+string2 = 'service_'
+unique_service2=[string2 + x for x in unique_service]
+# flag
+unique_flag=sorted(df.flag.unique())
+string3 = 'flag_'
+unique_flag2=[string3 + x for x in unique_flag]
+# put together
+dumcols=unique_protocol2 + unique_service2 + unique_flag2
+print(dumcols)
+
+
+#do same for test set
+unique_service_test=sorted(df_test.service.unique())
+unique_service2_test=[string2 + x for x in unique_service_test]
+testdumcols=unique_protocol2 + unique_service2_test + unique_flag2
+
+df_categorical_values_enc=df_categorical_values.apply(LabelEncoder().fit_transform)
+print(df_categorical_values_enc.head())
+# test set
+testdf_categorical_values_enc=testdf_categorical_values.apply(LabelEncoder().fit_transform)
+
+enc = OneHotEncoder()
+df_categorical_values_encenc = enc.fit_transform(df_categorical_values_enc)
+df_cat_data = pd.DataFrame(df_categorical_values_encenc.toarray(),columns=dumcols)
+# test set
+testdf_categorical_values_encenc = enc.fit_transform(testdf_categorical_values_enc)
+testdf_cat_data = pd.DataFrame(testdf_categorical_values_encenc.toarray(),columns=testdumcols)
+
+df_cat_data.head()
+
+trainservice=df['service'].tolist()
+testservice= df_test['service'].tolist()
+difference=list(set(trainservice) - set(testservice))
+string = 'service_'
+difference=[string + x for x in difference]
+difference
+
+
+for col in difference:
+    testdf_cat_data[col] = 0
+
+testdf_cat_data.shape
+
+
+#train data
+newdf=df.join(df_cat_data)
+newdf.drop('flag', axis=1, inplace=True)
+newdf.drop('protocol_type', axis=1, inplace=True)
+newdf.drop('service', axis=1, inplace=True)
+
+
+# test data
+newdf_test=df_test.join(testdf_cat_data)
+newdf_test.drop('flag', axis=1, inplace=True)
+newdf_test.drop('protocol_type', axis=1, inplace=True)
+newdf_test.drop('service', axis=1, inplace=True)
+print(newdf.shape)
+print(newdf_test.shape)
+
+
+print(newdf.head())
+
+
+# take label column
+labeldf=newdf['label']
+labeldf_test=newdf_test['label']
+# change the label column
+
+newlabeldf=labeldf.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+newlabeldf_test=labeldf_test.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+# put the new label column back
+newdf['label'] = newlabeldf
+newdf_test['label'] = newlabeldf_test
+print(newdf['label'].head())
+
+
+
+X_train = newdf[[i for i in list(newdf.columns) if i != 'label']]
+Y_train = newdf.loc[:,'label']
+
+print(X_train)
+
+print(Y_train)
+
+
+X_test = newdf_test[[i for i in list(newdf.columns) if i != 'label']]
+Y_test = (newdf_test).loc[:,'label']
+
+
+print(X_test)
+
+print(Y_test)
+
+
+
+
+import keras
+from numpy import array
+from numpy import argmax
+from keras.utils import to_categorical
+
+
+encoded = to_categorical(Y_train)
+print(encoded)
+
+Y_train2 = pd.DataFrame(encoded, columns=['normal', 'dos','probe','R2L','U2L'])
+
+encoded1 = to_categorical(Y_test)
+print(encoded1)
+
+Y_test2 = pd.DataFrame(encoded, columns=['normal', 'dos','probe','R2L','U2L'])
+
+
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Dense(output_dim = 61, init = 'uniform' , activation = 'relu', input_dim = 122 ))
+
+classifier.add(Dense(output_dim = 61, init = 'uniform' , activation = 'relu'))
+
+classifier.add(Dense(output_dim = 5, init = 'uniform' , activation = 'softmax'))
+
+classifier.compile(optimizer='adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+
+classifier.fit(X_train,Y_train2, batch_size = 10 , epochs = 5)
